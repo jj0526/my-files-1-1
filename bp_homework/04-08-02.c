b3a38b74c95bbdbc1eb533a1605b27adecb35ce5
jj0526 <wjstlgus1@naver.com>
2021-04-08 22:37:52 +0900

    2021-04-08-03

--- bp_homework/04-08-02.c
+++ bp_homework/04-08-02.c
@@ -1,28 +1,57 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
-    
+int main()
+{
+
     int num_nine = 0;
     int num;
-    
+
     printf("number?");
     scanf("%d", &num);
 
-    for (int i = 0; i<4; i++)
+    if (0 < num && num < 100000)
     {
-        if (num / pow(10,4-i) == 9)
+
+        for (int i = 0; i < 5; i++)
         {
-            num_nine++;
-            num = num - (num / pow(10,4-i));
+            if (num % 10 == 9)
+            {
+                num_nine++;
+                num = num / 10;
+            }
+            else
+            {
+                num = num / 10;
+            }
         }
-        else
-        {
-            num = num % (int)(pow(10,4-i));
-        }
+        printf("%d", num_nine);
     }
-    
-    printf("%d",num_nine);
-    
+    else if (-100000 < num && num < 0)
+    {
+        num = num + 99999;
+        for (int i = 0; i < 5; i++)
+        {
+            if (num % 10 == 0)
+            {
+                num_nine++;
+                num = num / 10;
+            }
+            else
+            {
+                num = num / 10;
+            }
+        }
+        printf("%d", num_nine);
+    }
+    else if (num == 0)
+    {
+        printf("%d", num_nine);
+    }
+    else
+    {
+        printf("Type the number again.");
+    }
+
     return 0;
 }
---
+++ for_fun/2021-04-08-02.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    int a = 2;
+    a = a-5;
+    printf("%d",a);
+}
