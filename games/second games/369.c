6bc0010ce02b44c30906eb21fcb60915443e06b8
jj0526 <wjstlgus1@naver.com>
2021-04-29 20:35:46 +0900

    369_ver_1.2

--- games/second games/369.c
+++ games/second games/369.c
@@ -2,24 +2,25 @@
 #include <time.h>
 #include <math.h>
 
-int main(){
+int main()
+{
 
-    int player[1000];
-    int first;
+    int player[1000];                //what player typed
+    int first;                       // starting first or second
     int dig;
-    int dig_count;
+    int dig_count;                   // count of i
     int temp;
-    int num_c = 0;
-    char first_c[1000];
-    char second_c[1000];
-    char third_c[1000];
-    int score;
-    int jari[100];
-    int the_num;
+    int num_c = 0;                   //how many Cs
+    char first_c[1000];              //<c>
+    char second_c[1000];             //c<c>
+    char third_c[1000];              //cc<c>
+    int score;                       //score
+    int jari[100];                   //count of digits
+    int the_num;                     // i
 
     printf("0 for starting first. 1 for starting second\n");
 
-    scanf("%d",&first);
+    scanf("%d", &first);
 
     int i = 0;
     int count = 0;
@@ -33,22 +34,26 @@ int main(){
 
             dig_count = 0;
 
-            the_num = i+1;
+            the_num = i + 1;
 
-            for ( int j = 0 ; j<3; j++)
+            for (int j = 0; j < 3; j++)
             {
                 the_num = the_num / 10;
-                if ( temp > 0 )
+
+                if (the_num > 0)
                 {
                     dig_count++;
                 }
             }
-            the_num = i+1;
-            for (int j = 0; j<dig_count+1; j++)
+
+            the_num = i + 1;
+
+            for (int j = 0; j < dig_count + 1; j++)
             {
-                jari[j] = the_num%10;
-                the_num = the_num/10;
-                if (jari[j]%3==0)
+                jari[j] = the_num % 10;
+                the_num = the_num / 10;
+
+                if (jari[j] % 3 == 0)
                 {
                     num_c++;
                 }
@@ -56,7 +61,7 @@ int main(){
             if (num_c == 0)
             {
                 scanf("%d", player[i]);
-                if (player[i]==i+1)
+                if (player[i] == i + 1)
                 {
                     continue;
                 }
@@ -65,10 +70,10 @@ int main(){
                     break;
                 }
             }
-            else if(num_c == 1)
+            else if (num_c == 1)
             {
-                scanf(" %c",&first_c[i]);
-                if (first_c[i] == 'c')
+                scanf(" %c", &first_c[i]);
+                if ((first_c[i] == 'c')&&(second_c[i]==NULL)&&(third_c[i]==NULL))
                 {
                     continue;
                 }
@@ -79,8 +84,8 @@ int main(){
             }
             else if (num_c == 2)
             {
-                scanf(" %c%c",&first_c[i],&second_c[i]);
-                if ((first_c[i]=='c')&&(second_c[i]=='c'))
+                scanf(" %c%c", &first_c[i], &second_c[i]);
+                if ((first_c[i] == 'c') && (second_c[i] == 'c')&&(third_c[i]==NULL))
                 {
                     continue;
                 }
@@ -91,8 +96,8 @@ int main(){
             }
             else if (num_c == 3)
             {
-                scanf(" %c%c%c",&first_c[i],&second_c[i],&third_c[i]);
-                if ((first_c[i]=='c')&&(second_c[i]=='c')&&(third_c[i]=='c'))
+                scanf(" %c%c%c", &first_c[i], &second_c[i], &third_c[i]);
+                if ((first_c[i] == 'c') && (second_c[i] == 'c') && (third_c[i] == 'c'))
                 {
                     continue;
                 }
@@ -100,49 +105,53 @@ int main(){
                 {
                     break;
                 }
-
             }
-            for (int j = 0; j<3; j++)
+            for (int j = 0; j < 3; j++)
             {
-                jari[j]=NULL;
+                jari[j] = NULL;
             }
 
             num_c = 0;
 
             dig_count = 0;
-            
+
             i++;
-            the_num = i+1;
-            for ( int j = 0 ; j<3; j++)
+            the_num = i + 1;
+            for (int j = 0; j < 3; j++)
             {
                 the_num = the_num / 10;
-                if ( temp > 0 )
+                if (temp > 0)
                 {
                     dig_count++;
                 }
             }
-            for (int j = 0; j<dig_count; j++)
+            the_num = i + 1;
+            for (int j = 0; j < dig_count; j++)
             {
-                jari[j] = the_num%10;
-                the_num = the_num/10;
-                if (jari[j]%3==0)
+                jari[j] = the_num % 10;
+                the_num = the_num / 10;
+                if (jari[j] % 3 == 0)
                 {
                     num_c++;
                 }
             }
             if (num_c == 0)
             {
-                printf("%d",i+1);
+                printf("%d", i + 1);
             }
-            else{
+            else
+            {
 
-                for (int j = 0; j<num_c; j++)
+                for (int j = 0; j < num_c; j++)
                 {
                     printf("c");
                 }
             }
+            for (int j = 0; j < 3; j++)
+            {
+                jari[j] = NULL;
+            }
             i++;
         }
     }
-
 }
