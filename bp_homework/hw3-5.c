db239ba2a15d55ce39e3caafcf3d99d38bf8d24f
jj0526 <wjstlgus1@naver.com>
2021-05-22 23:57:37 +0900

    histogram

--- bp_homework/hw3-5.c
+++ bp_homework/hw3-5.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
 int main(){
-    
+
+    int value[20];
+
+    for (int i = 0; i<10; i++){
+        scanf("%d", &value[i]);
+    }
+    printf("Element        Value      Histogram\n");
+    for (int i = 0; i<10; i++){
+        printf("      %d           ", i);
+        if (value[i]<10){
+            printf(" ");
+        }
+        printf("%d      ", value[i]);
+        for (int j = 0; j<value[i]; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+
 }
