5d0a501a992deb7f65d9cecbffcb4f035d8c2d51
jj0526 <wjstlgus1@naver.com>
2021-09-03 02:31:18 +0900

    put some comments

--- solving/1-1.c
+++ solving/1-1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    int age[5], scores[5];// makes each array a 5-element integer arrat
+    int age[5], scores[5];// makes each array a 5-element integer array
     int maxagegrp[5], minagegrp[5];
     int maxage = -1, minage = 1000, maxscore=-1, minscore=1000;
     int maxagecnt=0, minagecnt=0, maxscorecnt=0, minscorecnt=0;
@@ -9,7 +9,7 @@ int main(){
 
     for (int i = 0; i<5; i++){
         scanf("%d %d", &age[i], &scores[i]);
-        if ((age[i]<=0)||(scores[i]<=0)){
+        if ((age[i]<=0)||(scores[i]<=0)){//if age<=0(international age) or the score<=0
             printf("input the age and score again\n");
             i--;
         }
@@ -17,39 +17,39 @@ int main(){
     for (int i = 0; i<5; i++){
         if (maxage < age[i]){
             maxagecnt = 0;
-            maxage = age[i];
-            maxagegrp[maxagecnt] = i;
+            maxage = age[i];             // new max age found
+            maxagegrp[maxagecnt] = i;    //save the age index
         }
         else if(age[i]==maxage){
-            maxagecnt++;
-            maxagegrp[maxagecnt] = i;
+            maxagecnt++;                 //tie max age found
+            maxagegrp[maxagecnt] = i;    //save the age index
         }
         if (minage > age[i]){
             minagecnt = 0;
-            minage = age[i];
-            minagegrp[minagecnt] = i;
+            minage = age[i];             // new max age found
+            minagegrp[minagecnt] = i;    //save the age index
         }
         else if(minage==age[i]){ 
-            minagecnt++;
-            minagegrp[minagecnt] = i;
+            minagecnt++;                 //tie max age found
+            minagegrp[minagecnt] = i;    //save the age index
         }
         if ( maxscore < scores[i] ){
             maxscorecnt = 0;
-            maxscore = scores[i];
-            maxscoregrp[maxagecnt] = i;
+            maxscore = scores[i];        // new max age found
+            maxscoregrp[maxagecnt] = i;  //save the age index
         }
         else if (maxscore == scores[i]){
-            maxscorecnt++;
-            maxscoregrp[maxagecnt] = i;
+            maxscorecnt++;               //tie max age found
+            maxscoregrp[maxagecnt] = i;  //save the age index
         }
         if (minscore > scores[i]){
             minscorecnt = 0;
-            minscore = scores[i];
-            minscoregrp[minscorecnt] = i;
+            minscore = scores[i];         // new max age found
+            minscoregrp[minscorecnt] = i; //save the age index
         }
         else if (minscore == scores[i]){
-            minscorecnt++;
-            minscoregrp[minscorecnt] = i;
+            minscorecnt++;                //tie max age found
+            minscoregrp[minscorecnt] = i; //save the age index
         }
     }
     printf("%d %d %d %d\n", maxagecnt, minagecnt, maxscorecnt, minscorecnt);
